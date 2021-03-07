3c6656bc92d4704d61b64b44a3e1cf52fa45345b
Leandro <leandro.feitoza8018@gmail.com>
2021-03-06 21:40:42 -0300

    implementado mongoose

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 const multer = require('multer');
-const path = require('path');
 const multerConfig = require('./multer/multer');
+const Upload = require('./models/upload');
 
-const upload = multer(multerConfig);
+
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/test',
+  {
+    useNewUrlParser: true, useUnifiedTopology: true
+  }
+);
 
 router.get('/', (req, res) => res.send('Hello '));
 
-router.post('/', upload.single('file'), (req, res) => {
-  console.log(req.file)
-  res.status(200).json({up: 'load'})
+router.post('/', multer(multerConfig).single('file'), async (req, res) => {
+  const { originalname: name, size, filename: key } = req.file
+  const upload = await Upload.create({
+    name,
+    key,
+    size,
+    url: ''
+  })
+  res.status(200).json(upload)
 })
 
 module.exports = router;
